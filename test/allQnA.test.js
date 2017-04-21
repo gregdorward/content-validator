6970d25452b0a9c1a211a21fa234cf934cafada0
gregdorward <greg.dorward@red-badger.com>
2017-04-21 11:09:27 +0100

    Update allQnA.test.js

--- test/allQnA.test.js
+++ test/allQnA.test.js
@@ -5,24 +5,27 @@ var expect = chai.expect;
 describe('Get allQnA', () => {
   var qna;
   before((done) => {
-    fetch('http://brain.red-badger.com/graphql?query=%7B%0A%20%20allQnA%20%7B%0A%20%20%20%20name%0A%20%20%20%20slug%0A%20%20%20%20topics%20%7B%0A%20%20%20%20%20%20slug%0A%20%20%20%20%20%20question%0A%20%20%20%20%20%20answer%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A')
+    fetch('http://brain.red-badger.com/graphql?query=%7B%0A%20%20allQnA%20%7B%0A%20%20%20%20name%0A%20%20%20%20slug%0A%20%20%20%20topics%20%7B%0A%20%20%20%20%20%20slug%0A%20%20%20%20%20%20question%0A%20%20%20%20%20%20answer%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D')
         .then(res => res.json())
         .then(json => {qna = json.data.allQnA})
         .then(done);
   })
+  it('should not return an empty json object', () => {
+    expect(qna, 'empty response returned').to.not.be.empty;
+  })
   it('should not return any null values for Question categories', () => {
     qna.forEach((question) => {
-      expect(!!question.name).to.be.true;
+      expect(!!question.name, 'missing a category name').to.be.true;
     })
   })
   it('should not return any null values for Questions', () => {
     qna.forEach((question) => {
-    expect(!!question.question).to.be.true;
+    expect(!!question.question, 'missing a question').to.be.true;
     })
   })
   it('should not return any null values for Answers', () => {
     qna.forEach((question) => {
-    expect(!!question.answer).to.be.true;
+    expect(!!question.answer, 'missing an answer').to.be.true;
     })
   })
 })
